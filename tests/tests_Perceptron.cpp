f3e49d8f93f06d7a5354f21a206c5a352ee4d73c
Eric Lopez <pikurrot@gmail.com>
2023-11-26 14:35:50 +0100

    Solve bug error message not correctly showing source

--- tests/tests_Perceptron.cpp
+++ tests/tests_Perceptron.cpp
@@ -2,19 +2,51 @@
 #include "../deep_learning/Perceptron.hpp"
 #include "../core/linear_algebra.hpp"
 #include "../core/Mat.hpp"
+#include <string>
 
 void testPerceptronConstructor()
 {
-	Perceptron *p = new Perceptron(2);
+	Perceptron *p = nullptr;
+	try
+	{
+		p = new Perceptron(2);
+	}
+	catch (const char *e)
+	{
+		std::string msg = "Perceptron constructor failed: ";
+		msg += e;
+		delete p;
+		p = nullptr;
+		throw msg.c_str();
+	}
 	delete p;
 	p = nullptr;
 }
 
 void testPerceptronForward()
 {
-	Perceptron *p = new Perceptron(2);
-	Mat *inputs = random(1, 4);
-	float output = p->forward(inputs);
+	Perceptron *p = nullptr;
+	Mat *inputs = nullptr;
+	try
+	{
+		p = new Perceptron(4);
+		inputs = random(1, 4);
+		float output = p->forward(inputs);
+		if (output < 0 || output > 1)
+		{
+			throw "Perceptron output out of range";
+		}
+	}
+	catch (const char *e)
+	{
+		std::string msg = "Perceptron forward failed: ";
+		msg += e;
+		delete p;
+		p = nullptr;
+		delete inputs;
+		inputs = nullptr;
+		throw msg;
+	}
 	delete p;
 	p = nullptr;
 	delete inputs;
